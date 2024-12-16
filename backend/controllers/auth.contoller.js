5101271e54ce903152f32adc870d13d634870ab7
Ashutosh-7Singh <ashutosh.singh@thegoacademy.com>
2024-12-17 01:13:01 +0530

    updated

--- backend/controllers/auth.contoller.js
+++ backend/controllers/auth.contoller.js
@@ -1,5 +1,51 @@
+import { User } from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { generateVerificationToken } from "../utils/generateVerificationToken.js";
+import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
 export const signup = async (req, res) => {
-const {email,password,name}= req.body
+  const { email, password, name } = req.body;
+  // console.log(req.body);
+  
+  try {
+    if (!email || !password || !name) {
+      throw new Error("All fields are required ");
+    }
+    const userAlreadyExists = await User.findOne({ email });
+    console.log("userAlreadyExists",userAlreadyExists);
+    
+    if (userAlreadyExists) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exist" });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    const verificationToken = generateVerificationToken();
+    const user = new User({
+      email,
+      password: hashedPassword,
+      name,
+      verificationToken,
+      verificationTokenExpeiresAt: Date.now() + 24 * 60 * 60 * 1000, //24 hours
+    });
+    console.log("user",user);
+    
+    await user.save();
+    // jwt
+    generateTokenAndSetCookie(res, user._id);
+sendVerificationEmail(user.email,verificationToken)
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({ success: false, message: error.message });
+  }
 };
 
 export const login = async (req, res) => {
